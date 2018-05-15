ddabedc98f8975ac65f58d21df46a68028d920f9
Emil Folino <emilfolino@gmail.com>
2018-05-15 09:29:23 +0200

    Getting from proxy

--- js/models/polisen.js
+++ js/models/polisen.js
@@ -2,8 +2,9 @@ var m = require("mithril");
 
 var polisen = {
     get: function() {
+        console.log("get");
         return m.request({
-            url: "proxy.php",
+            url: "http://www.student.bth.se/~efostud/mithril_test/proxy.php",
             method: "GET"
         }).then(function(result) {
             console.log(result)
