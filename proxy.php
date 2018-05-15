d209cb1b618e204aa12354ec24a5185db39e95f6
Emil Folino <emilfolino@gmail.com>
2018-05-15 09:07:47 +0200

    Send json response

--- proxy.php
+++ proxy.php
@@ -1,20 +1,18 @@
 <?php
-
-print("proxy");
-
 // create curl resource
 $ch = curl_init();
-print("init");
+
 // set url
 curl_setopt($ch, CURLOPT_URL, "https://polisen.se/api/events?locationname=Karlskrona");
-print("setopt");
+
 //return the transfer as a string
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-print("setopt");
+
 // $output contains the output string
 $output = curl_exec($ch);
-var_dump($output);
+
 // close curl resource to free up system resources
 curl_close($ch);
 
-print("HEJ HOPP I'm done");
+header('Content-type: application/json');
+echo $output;
