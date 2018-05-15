b927cac24b80e6bd5d7a175724e83d504cbdc7e4
Emil Folino <emilfolino@gmail.com>
2018-05-15 09:10:57 +0200

    updated to get from proxy

--- js/models/polisen.js
+++ js/models/polisen.js
@@ -2,9 +2,8 @@ var m = require("mithril");
 
 var polisen = {
     get: function() {
-        console.log("hej hopp");
         return m.request({
-            url: "https://polisen.se/api/events?locationname=Blekinge;Karlskrona",
+            url: "proxy.php",
             method: "GET"
         }).then(function(result) {
             console.log(result)
